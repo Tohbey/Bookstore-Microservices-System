2cc5598f852d0e0f5b2552224b13833e8d299adb
Oluwatobiloba <33703095+Tohbey@users.noreply.github.com>
2025-06-07 23:29:14 +0100

    bug fixes on repository test

--- AuthorService/src/test/java/com/bookstore/authorservice/repository/BookRepositoryTest.java
+++ AuthorService/src/test/java/com/bookstore/authorservice/repository/BookRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ActiveProfiles("test")
 @DataJpaTest
 class BookRepositoryTest {
 
